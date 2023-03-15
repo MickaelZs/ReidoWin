2eac7107492b8db42e2294ba30e9683c860b01c1
EduardoPH <dudu19424irma@gmail.com>
2023-03-15 00:28:47 -0300

    Criação componente novo

--- src/components/bet/styled.js
+++ src/components/bet/styled.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   /* *{border:solid 2px red;} */
+  margin: 1em 0 2em;
   .form {
     position: relative;
     display: flex;
@@ -10,13 +11,27 @@ export const Container = styled.div`
     align-items: center;
     background-color: transparent;
 
-    span {
-      font-family: 'Inter';
-      font-style: normal;
-      font-weight: 800;
-      font-size: 30px;
-      line-height: 36px;
+    .index-trash {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      span {
+        font-family: 'Inter';
+        font-style: normal;
+        font-weight: 800;
+        font-size: 30px;
+        line-height: 36px;
+        margin-bottom: 0.3em;
+      }
+
+      img {
+        width: 30px;
+      }
+
+      margin-right: 4em;
     }
+
+    
   }
 
   .input-group {
@@ -24,7 +39,6 @@ export const Container = styled.div`
     flex-wrap: wrap;
     justify-content: space-between;
     width: 100%;
-  
   }
 
 `;
--- src/components/input/index.jsx
+++ src/components/input/index.jsx
@@ -1,15 +1,26 @@
 import {Container, Input} from './style.js'
 export default function InputBoxc(props) {
 
-  const {label, isBackground, isCurrency, isProfit} = props;
+  const {label, isBackground, isCurrency, isProfit, index} = props;
+
+  function limitDecimalPlaces(e, count) {
+    if (e.target.value.indexOf('.') == -1) { return; }
+    if ((e.target.value.length - e.target.value.indexOf('.')) > count) {
+      e.target.value = parseFloat(e.target.value).toFixed(count);
+    }
+  }
+  
   return (
     <Container> 
-      <span> {label} </span>
+      <span> {index <= 0 && label} </span>
       <Input 
         type={isCurrency ? 'text' : 'number'} 
-        style={{backgroundColor: isBackground ? "#149A00" : '', color: isProfit ? "#FFF" : ''}} 
-        disabled={isProfit}
-        value={isCurrency && 'BRL'}
+        style={{backgroundColor: isBackground && "#149A00", color: isProfit ? "#FFF" : isCurrency && "#149A00", fontWeight: isCurrency && 800}} 
+        disabled={isProfit || isCurrency}
+        value={isCurrency && 'BRL$'}
+        step="0.01"
+        min="0"
+        onInput={e => limitDecimalPlaces(e, 2)}
       />
     </Container>
   )
