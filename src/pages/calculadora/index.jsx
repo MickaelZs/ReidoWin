3c49f5428b7fdacb49cdbb32cab428b9d1a1a21b
EduardoPH <dudu19424irma@gmail.com>
2023-03-14 21:02:37 -0300

    Remocao card antigo

--- src/pages/calculadora/index.jsx
+++ src/pages/calculadora/index.jsx
@@ -1,15 +1,13 @@
 
-import Card from "../../components/card"
 import Input from "../../components/input"
 import { Container } from "./styled"
 
 export default function Calculadora (){
     return(
         <Container>
-            <div>
-            <Card/>
+            <div className="box-content">
+                <Input />
             </div>
-           
         </Container>
 
     )
--- src/pages/calculadora/styled.js
+++ src/pages/calculadora/styled.js
@@ -1,5 +1,14 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #000;
+  height: 100vh;
+  align-items: center;
 
+  .box-content {
+    background-color: #fff;
+    width: 80%;
+  }
 `
