a2eb6e477f134e55fc4ceac5c7060535e8315cb2
Mickael <mickaelcosta431@gmail.com>
2023-03-15 00:50:11 -0300

    ok

--- src/components/bet/styled.js
+++ src/components/bet/styled.js
@@ -11,20 +11,42 @@ export const Container = styled.div`
     background-color: transparent;
 
     span {
-      font-family: 'Inter';
+      font-family: "Inter";
       font-style: normal;
       font-weight: 800;
       font-size: 30px;
       line-height: 36px;
     }
+    @media screen and (max-width: 320px) {
+      span {
+        font-size: 20px;
+      }
+      
+    } @media screen and (max-width: 480px) {
+        span {
+          font-size: 20px;
+        }
+        
+      } @media screen and (max-width: 720px) {
+        span {
+          font-size: 22px;
+        }
+        
+      }
   }
-
   .input-group {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
     width: 100%;
-  
   }
 
+  @media screen and (max-width: 480px) {
+    .input-group {
+      display: flex;
+
+      justify-content: center;
+      align-items: center;
+    }
+  }
 `;
--- src/components/input/style.js
+++ src/components/input/style.js
@@ -8,15 +8,16 @@ export const Container = styled.div`
     margin: 7px 0 1.6em;
   }
   font-size: 20px;
-  font-family: 'Inter';
+  font-family: "Inter";
   font-weight: 800;
-`
+  
+`;
 
 export const Input = styled.input`
-::-webkit-inner-spin-button{
-  -webkit-appearance: none;
-}
-  font-family: 'Inter';
+  ::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+  }
+  font-family: "Inter";
   font-weight: 500;
   font-size: 18px;
   -moz-appearance: textfield;
@@ -26,6 +27,22 @@ export const Input = styled.input`
   left: 602px;
   top: 246px;
   padding: 1em;
-  border: 1px solid #B8B5B5;
+  border: 1px solid #b8b5b5;
   border-radius: 15px;
-` 
+
+  @media screen and (max-width: 320px) {
+    width: 200px;
+    height: 10px;
+  }
+  @media screen and (max-width: 480px) {
+    display: flex;
+    justify-content: center;
+    width: 200px;
+    height: 10px;
+  } @media screen and (max-width: 720px) {
+    display: flex;
+    justify-content: center;
+    width: 200px;
+    height: 10px;
+  }
+`;
